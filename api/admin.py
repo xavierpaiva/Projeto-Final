cfaff921e3867db7f02ab060539c8b7fade7f235
Xavierpaiva <Xavier.paiva@escolar.ifrn.edu.br>
2023-12-22 08:32:04 -0300

    .

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from .models import Artista, Album, Musica
+
+admin.site.register(Artista)
+admin.site.register(Album)
+admin.site.register(Musica)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Artista(models.Model):
+    nome = models.CharField(max_length=100)
+    local = models.CharField(max_length=100)
+    ano_criacao = models.IntegerField()
+
+def __str__(self):
+    return self.nome
+
+class Album(models.Model):
+    artista = models.ForeignKey(Artista, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100)
+    ano = models.IntegerField()
+
+def __str__(self):
+    return self.nome
+
+class Musica(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100)
+    segundos = models.IntegerField()
+
+def __str__(self):
+    return self.nome
